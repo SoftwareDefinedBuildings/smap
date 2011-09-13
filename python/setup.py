d4419b596f858479ff046095d004d110fb563c39
sdhags <sdhags@511b6515-856b-012f-224d-303ca1f27614>
2011-09-13 00:43:05 +0000

    (properly install subpackages)
    git-svn-id: http://smap-data.googlecode.com/svn/branches/v2@57 511b6515-856b-012f-224d-303ca1f27614

--- python/setup.py
+++ python/setup.py
@@ -4,11 +4,19 @@ import glob
 
 setup(name="Smap",
       version="1.1",
-      description="sMAP standard library",
+      description="sMAP standard library and drivers",
       author="Stephen Dawson-Haggerty",
       author_email="stevedh@eecs.berkeley.edu",
-      packages=["smap"],
+      packages=["smap", # "smap.drivers", 
+                "smap.contrib",
+                "smap.iface", "smap.iface.http", "smap.iface.modbus",
+                "smap.drivers.obvius",
+                # packages for the acme driver -- don't install this in trunk/
+                "smap.drivers.acmex2", "tinyos", "tinyos.message",
+                # hack to support ipv6 sockets -- needed for acme, at least
+                "tx", "tx.ipv6", "tx.ipv6.application", "tx.ipv6.internet"
+                ],
       requires=["avro", "twisted"],
-      package_dir={'smap' : 'smap'},
+      package_dir={"smap" : "smap"},
       package_data={"smap" : ['schema/*.av']},
       scripts=['bin/smap-run-driver', 'bin/smap-run-conf'])
