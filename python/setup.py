644ff399c6910b1d183181e7f9a421fe0b3cb705
sdhags <sdhags@511b6515-856b-012f-224d-303ca1f27614>
2011-09-14 21:11:02 +0000

    add twistd plugin so the sMAP server can be run as a daemon very easily
    git-svn-id: http://smap-data.googlecode.com/svn/branches/v2@61 511b6515-856b-012f-224d-303ca1f27614

--- python/setup.py
+++ python/setup.py
@@ -7,17 +7,28 @@ setup(name="Smap",
       description="sMAP standard library and drivers",
       author="Stephen Dawson-Haggerty",
       author_email="stevedh@eecs.berkeley.edu",
-      packages=["smap", 
-                "smap.drivers", 
-                "smap.contrib",
-                "smap.archiver",
-                "smap.iface", "smap.iface.http", "smap.iface.modbus",
-                "smap.drivers.obvius",
-                # packages for the acme driver -- don't install this in trunk/
-                "smap.drivers.acmex2", "tinyos", "tinyos.message",
-                # hack to support ipv6 sockets -- needed for acme, at least
-                "tx", "tx.ipv6", "tx.ipv6.application", "tx.ipv6.internet"
-                ],
+      packages=[
+        # core sMAP libs and drivers
+        "smap", 
+        "smap.drivers", 
+        "smap.contrib",
+
+        # smap archiver components
+        "smap.archiver",
+
+        "twisted", "twisted.plugins",
+
+        # interfaces for talking to different backends
+        "smap.iface", "smap.iface.http", "smap.iface.modbus",
+
+        # hack to support ipv6 sockets -- needed for acme, at least
+        "tx", "tx.ipv6", "tx.ipv6.application", "tx.ipv6.internet",
+
+        # various extra divers and dependencies -- might want to leave this out on mainline
+        "smap.drivers.obvius",
+        # packages for the acme driver -- don't install this in trunk/
+        "smap.drivers.acmex2", "tinyos", "tinyos.message",
+        ],
       requires=["avro", "twisted"],
       package_dir={"smap" : "smap"},
       package_data={"smap" : ['schema/*.av']},
---
+++ python/twisted/plugins/smap_plugin.py
@@ -0,0 +1,37 @@
+
+import sys
+
+from zope.interface import implements
+
+from twisted.python import usage
+from twisted.plugin import IPlugin
+from twisted.application.service import IServiceMaker
+from twisted.application import internet
+
+from smap import core, loader, smapconf
+from smap.server import getSite
+
+class Options(usage.Options):
+    optParameters = [["conf", "c", None, "sMAP configuration file"],
+                     ["port", "p", None, "service port number"]]
+
+class SmapServiceMaker(object):
+    implements(IServiceMaker, IPlugin)
+    tapname = "smap"
+    description = "A sMAP server"
+    options = Options
+
+    def makeService(self, options):
+        if options['conf'] == None:
+            print >>sys.stderr, "FATAL: Must provide a sMAP config file (use -c)"
+            sys.exit(1)
+
+        inst = loader.load(options['conf'])
+        if options["port"] != None:
+            port = options["port"]
+        else:
+            port = smapconf.SERVER['Port']
+
+        return internet.TCPServer(int(port), getSite(inst))
+
+serviceMaker = SmapServiceMaker()
