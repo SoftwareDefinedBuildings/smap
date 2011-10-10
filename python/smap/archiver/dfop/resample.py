925109a4958248bdc083b6dc464a5941e14b6a79
sdhags@gmail.com <sdhags@gmail.com@511b6515-856b-012f-224d-303ca1f27614>
2011-10-10 00:11:21 +0000

    - fix the summing (sort of?)
    git-svn-id: http://smap-data.googlecode.com/svn/branches/v2@86 511b6515-856b-012f-224d-303ca1f27614

--- python/smap/archiver/dfop/resample.py
+++ python/smap/archiver/dfop/resample.py
@@ -1,4 +1,5 @@
 
+import unittest
 import operator
 
 def data_parallel(op):
@@ -14,17 +15,51 @@ def data_agg(op):
         return [op(streams)]
     return real_op
 
-def read_aligned(streams):
+def mmin(row):
+    try:
+        return min((v[0] for v in row if v is not None))
+    except ValueError:
+        return float('-inf')
+
+def mmax(row):
+    try:
+        return min((v[0] for v in row if v is not None))
+    except ValueError:
+        return float('inf')
+
+def read_aligned(streams, ignore_missing=True):
     """read a set of records from a stream vector by reading points
     util a tuple is found where all the timestamps are the same.
     """
-    row = map(lambda x: x.next(), streams)
-    times = map(operator.itemgetter(0), row)
-    while min(times) != max(times):
-        idx = times.index(min(times))
-        row[idx] = streams[idx].next()
-        times[idx] = row[idx][0]
+    idx = 0
+    row = [None] * len(streams)
+    while (None in row or mmin(row) != mmax(row)):
+        try:
+            if row[idx] == None or row[idx][0] == mmin(row):
+                row[idx] = streams[idx].next()
+        except StopIteration:
+            del streams[idx]
+            del row[idx]
+        if len(streams) == 0:
+            break
+        idx = (idx + 1) % len(streams)
     return row
+    
+#     idxs = range(0, len(streams))
+#     if ignore_missing:
+#         for i in xrange(0, len(streams)):
+#             try:
+#                 row[i] = streams[i].next()
+#             except StopIteration:
+#                 idxs[i] = -1
+#     else:
+#         times = map(operator.itemgetter(0), row)
+        
+#     while min(times) != max(times):
+#         idx = times.index(min(times))
+#         row[idx] = streams[idx].next()
+#         times[idx] = row[idx][0]
+#     return row
 
 @data_parallel
 def decimate(n):
@@ -63,11 +98,14 @@ def count():
     return fn
 
 @data_agg
-def dfsum(streams):
+def dfsum(streams, ignore_missing=True):
     while 1:
-        row = read_aligned(streams)
-        val = reduce(operator.__add__, map(operator.itemgetter(1), row), 0)
-        yield (row[0][0], val)
+        row = read_aligned(streams, ignore_missing)
+        if len(row) > 0:
+            val = reduce(operator.__add__, map(operator.itemgetter(1), row), 0)
+            yield (row[0][0], val)
+        else:
+            break
 
 operators = {
     'count' : (count, []),
@@ -77,16 +115,42 @@ operators = {
     }
 
 
+class TestReadAligned(unittest.TestCase):
+    def makeStream(self, start, end, step=1):
+        return ((x,) for x in xrange(start, end, step))
+    
+#     def testSimple(self):
+#         streams = [self.makeStream(0, 10) for x in range(0, 5)]
+#         for i in xrange(0, 10):
+#             row = read_aligned(streams)
+#             for j in xrange(0, 5):
+#                 self.assertEquals(row[j][0], i)
+
+    def testMissing(self):
+        streams = [self.makeStream(0, 10) for x in range(0, 4)]
+        streams += [self.makeStream(0, 5)]
+        for i in xrange(0, 5):
+            row = read_aligned(streams)
+            for j in xrange(0, 5):
+                self.assertEquals(row[j][0], i)
+
+        for i in xrange(5, 10):
+            row = read_aligned(streams)
+            for j in xrange(0, 4):
+                self.assertEquals(row[j][0], i)
+
+
 if __name__ == '__main__':
-    def data(start):
-        for x in xrange(start, 1000):
-            yield (x, x)
+    unittest.main()
+#     def data(start):
+#         for x in xrange(start, 1000):
+#             yield (x, x)
 
-    streams = [data(0), data(10)]
-    op = sum(subsample(streams, 50))
-    print op
+#     streams = [data(0), data(10)]
+#     op = sum(subsample(streams, 50))
+#     print op
 
-    print map(list, op)
+#     print map(list, op)
 
-#     print list(decimate(10)(data()))
-#     print list(subsample(10)(data()))
+# #     print list(decimate(10)(data()))
+# #     print list(subsample(10)(data()))
