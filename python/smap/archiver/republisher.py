7c99fc7030c54d383591c872b16662063fb13690
sdhags <sdhags@511b6515-856b-012f-224d-303ca1f27614>
2011-12-07 19:25:17 +0000

    - add streaming republishing convenience resource
    git-svn-id: http://smap-data.googlecode.com/svn/trunk@108 511b6515-856b-012f-224d-303ca1f27614

---
+++ python/smap/archiver/republisher.py
@@ -0,0 +1,31 @@
+
+from twisted.web import resource, server
+
+import smap.util as util
+
+def cl(*args):
+    print "connection lost"
+    print args
+
+
+class ReResource(resource.Resource):
+    def __init__(self):
+        resource.Resource.__init__(self)
+        self.listeners = []
+
+    def connectionLost(self, request, reason):
+        self.listeners.remove(request)
+
+
+    def render_GET(self, request):
+        # make sure we remove the client when she disconnects
+        request.notifyFinish().addErrback(lambda x: self.connectionLost(request, x))
+        self.listeners.append(request)
+        return server.NOT_DONE_YET
+
+
+    def republish(self, obj):
+        data = util.json_encode(obj)
+        for client in self.listeners:
+            print client.write(data)
+            print client.write("\n\n")
