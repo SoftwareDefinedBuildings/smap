d755015590e1cc79fb255b65c22a51c2bfe9e5f5
sdhags@gmail.com <sdhags@gmail.com@511b6515-856b-012f-224d-303ca1f27614>
2012-10-22 03:35:20 +0000

    ReadingDB driver/example conf
    git-svn-id: http://smap-data.googlecode.com/svn/trunk@415 511b6515-856b-012f-224d-303ca1f27614

--- python/smap/drivers/readingdb.py
+++ python/smap/drivers/readingdb.py
@@ -1,87 +1,63 @@
+"""
+Copyright (c) 2011, 2012, Regents of the University of California
+All rights reserved.
 
-import sys
-import logging
-import time
-import threading
-import socket
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions 
+are met:
+
+ - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+ - Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the
+   distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+"""
+@author Stephen Dawson-Haggerty <stevedh@eecs.berkeley.edu>
+"""
+"""Driver for receiving statistics from a readingdb server
+"""
+
 import json
 
-sys.path.append("../../newlib")
-from SmapPoint import SmapPoint, Formatting, Parameter, Reading
-import SmapHttp
-import SmapInstance
-import smaplog
-RATE=5
+from twisted.internet.protocol import DatagramProtocol
+from tx.ipv6.internet import reactor
 
-class UpdateThread(threading.Thread):
-    def __init__(self, inst, leaf, port, rate=5):
-        threading.Thread.__init__(self)
-        self.setDaemon(True)
-        self.inst = inst
-        self.leaf = leaf
-        self.port = port
-        self.rate = rate
+from smap import driver
 
-        self.data = {}
-        self.last = time.time()
+class Driver(driver.SmapDriver, DatagramProtocol):
+    """Listen for statistics on whatever port from the readingdb
+server; readingdb sends statistics on the IPv6/UDP port that clients
+connect on.  They are sent as a json dict; this driver parses the json
+and adds them as as a sMAP data source.
+    """
+    stats = ['adds', 'connects', 'disconnects', 
+             'failed_add', 'nearest', 'queries']
 
-    def update(self, new):
-        timestamp = new['timestamp']
-        del new['timestamp']
-        for k,v in new.iteritems():
-            self.data[k] = self.data.get(k, 0) + v
-        if timestamp > self.last + self.rate:
-            print "publishing", self.data
-            for k, v in self.data.iteritems():
-                if self.leaf.has_key(k):
-                    self.leaf[k].add(Reading(time=timestamp,value=v,min=None,max=None))
-            self.inst.push()
-            self.data = {}
-            self.last = timestamp
+    def setup(self, opts):
+        for s in self.stats:
+            self.add_timeseries('/' + s, 'count')
+        self.port = int(opts.get('Port', 4242))
 
-    def run(self):
-        s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, 0)
-        s.bind(("::1", self.port))
-        while True:
-            stats = s.recv(1024)
-            data = json.loads(stats)
-            self.update(data)
+    def start(self):
+        reactor.listenUDP6(self.port, self)
 
-def get_tree(key):
-    return  {
-        key : {
-            'sensor' : {
-                'queries' : SmapPoint(Formatting(unit="count", multiplier=None, divisor=RATE,
-                                                 type="statistic", ctype="sensor"),
-                                      Parameter(interval=1, time='second')),
-                'adds' : SmapPoint(Formatting(unit="count", multiplier=None, divisor=RATE,
-                                                 type="statistic", ctype="sensor"),
-                                      Parameter(interval=1, time='second')),
-                'failed_adds': SmapPoint(Formatting(unit="count", multiplier=None, divisor=RATE,
-                                                 type="statistic", ctype="sensor"),
-                                      Parameter(interval=1, time='second')),
-                'connects' : SmapPoint(Formatting(unit="count", multiplier=None, divisor=RATE,
-                                                 type="statistic", ctype="sensor"),
-                                       Parameter(interval=1, time='second')),
-                'disconnects' : SmapPoint(Formatting(unit="count", multiplier=None, divisor=RATE,
-                                                 type="statistic", ctype="sensor"),
-                                      Parameter(interval=1, time='second')),
-                'nearest' : SmapPoint(Formatting(unit="count", multiplier=None, divisor=RATE,
-                                                 type="statistic", ctype="sensor"),
-                                      Parameter(interval=1, time='second'))
-                }
-            }
-        }
-
-if __name__ == '__main__':
-    smaplog.start_log()
-
-    host, port = "jackalope", 4246
-    key = "%s_%i" % (host, port)
-    port = 4242
-    tree = get_tree(key)
-    inst = SmapInstance.SmapInstance(tree, key="readingdb")
-    u = UpdateThread(inst, inst['data'][key]['sensor'], port)
-    u.start()
-
-    SmapHttp.start_server(inst, port=7012)
+    def datagramReceived(self, data, addr):
+        data = json.loads(data)
+        for stat, v in data.iteritems():
+            if not stat in self.stats: continue
+            self._add('/' + stat, data['timestamp'], v)
