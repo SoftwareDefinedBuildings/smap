fc62666027de794029e1029fffea99d3e394059b
sdhags@gmail.com <sdhags@gmail.com@511b6515-856b-012f-224d-303ca1f27614>
2012-12-18 00:30:05 +0000

    * update obvious driver to use FetchDriver infrastructure.  This should resolve the issue we were seeing that we might be hitting http://bugs.python.org/issue11108 since we'll now do the parsing in the reactor thread instead of the threadpool
    git-svn-id: http://smap-data.googlecode.com/svn/trunk@466 511b6515-856b-012f-224d-303ca1f27614

--- python/smap/drivers/obvius/obvius.py
+++ python/smap/drivers/obvius/obvius.py
@@ -30,35 +30,25 @@ OF THE POSSIBILITY OF SUCH DAMAGE.
 @author Stephen Dawson-Haggerty <stevedh@eecs.berkeley.edu>
 """
 
-import sys
 import logging
 import time
 import calendar
-import threading
-import urllib2
 import urlparse
 import re
-import signal
 
 import smap.driver
 from smap.util import periodicSequentialCall
 
 import sensordb
 
-urllib2.install_opener(urllib2.build_opener())
-
 def to_pathname(s):
     s = re.sub('[\W/]+', '_', s)
     s = re.sub('_*$', '', s)
     return s.lower()
 
-class Driver(smap.driver.SmapDriver):
+class Driver(smap.driver.FetchDriver):
     def setup(self, opts):
-        self.url = opts['Url']
-        self.auth = (opts['Username'], opts['Password'])
         self.mappings = [x for x in sensordb.DB if x['obviusname'] == opts['ObviusType']][0]
-        self.period = opts.get('Period', 30)
-
         self.set_metadata('/', {
                 'Extra/Driver' : 'smap.drivers.obvius',
                 'Instrument/Model' : opts['ObviusType']
@@ -71,58 +61,46 @@ class Driver(smap.driver.SmapDriver):
             self.set_metadata('/' + phase, {
                 'Extra/Phase' : phase })
 
+        # create the url with auth included
+        url_p = urlparse.urlparse(opts['Url'])
+        netloc = '%s:%s@%s' % (opts['Username'], opts['Password'], url_p.netloc)
+        url = urlparse.urlunparse((url_p.scheme, netloc, url_p.path, 
+                                   url_p.params, url_p.query, url_p.fragment))
 
-    def open_url(self, url, AUTH):
-        """Open a URL using urllib2, sending HTTP Basic authentication"""
-        mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        url_p = urlparse.urlparse(url)
-        mgr.add_password(None, url_p.netloc, AUTH[0], AUTH[1])
-        handler = urllib2.HTTPBasicAuthHandler(mgr)
-        opener = urllib2.build_opener(handler)
-        return opener.open(url, timeout=15)
+        smap.driver.FetchDriver.setup(self, {
+                'Uri' : url,
+                'Rate' : opts.get('Period', 30),
+                })
 
     def is_sensor(self, channel, OBVIUS_SENSORS):
         for (x,y,z,p,q) in OBVIUS_SENSORS:
             if p == channel:
                 return True
         return False
-
-    def start(self):
-        periodicSequentialCall(self.update).start(self.period)
     
-    def update(self):
-        logging.debug("Updating " + self.url)
+    def process(self, html):
+        # the obvius timestamp is in UTC
+        reading_time = re.findall('<time.*?>(.*?)</time>', html)[0]
+        # pydoc says this is the right way to convert this timestamp
+        reading_time = time.strptime(reading_time + " UTC",
+                                     "%Y-%m-%d %H:%M:%S %Z")
+        reading_time = calendar.timegm(reading_time)
+        logging.debug("reading timestamp is " + str(reading_time))
+        
+        # this pulls out a list of all the channel-reading pairs
+        data = re.findall('<point.*?name="(.*?)".*?value="(.*?)".*?/>', html.lower())
+        data_map = {}
+        data_map.update(data)
 
-        try:
-            fp = self.open_url(self.url, self.auth)
-            html = fp.read()
-            fp.close()
-
-            # the obvius timestamp is in UTC
-            reading_time = re.findall('<time.*?>(.*?)</time>', html)[0]
-            # pydoc says this is the right way to convert this timestamp
-            reading_time = time.strptime(reading_time + " UTC",
-                                         "%Y-%m-%d %H:%M:%S %Z")
-            reading_time = calendar.timegm(reading_time)
-            logging.debug("reading timestamp is " + str(reading_time))
-            
-            # this pulls out a list of all the channel-reading pairs
-            data = re.findall('<point.*?name="(.*?)".*?value="(.*?)".*?/>', html.lower())
-            data_map = {}
-            data_map.update(data)
-
-            for (field, regexp, phase, channel,fmt) in \
-                    self.mappings['sensors'] + self.mappings['meters']:
-                reading = data_map.get(field.lower())
-                if not reading: 
-                    logging.warn(field + " not found in doc")
-                    continue
-                match = re.search(regexp, reading)
-                if not match: 
-                    logging.warn("reading conversion failed: " + reading + " : " + field)
-                    continue
-                self.add('/' + phase + '/' + channel, reading_time, float(match.groups(0)[0]))
-
-        except IOError, e:
-            logging.error("Failed to load meter: %s: %s" % (self.url, str(e)))
+        for (field, regexp, phase, channel,fmt) in \
+                self.mappings['sensors'] + self.mappings['meters']:
+            reading = data_map.get(field.lower())
+            if not reading: 
+                logging.warn(field + " not found in doc")
+                continue
+            match = re.search(regexp, reading)
+            if not match: 
+                logging.warn("reading conversion failed: " + reading + " : " + field)
+                continue
+            self.add('/' + phase + '/' + channel, reading_time, float(match.groups(0)[0]))
 
