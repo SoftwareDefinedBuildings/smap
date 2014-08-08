4d2f05594fb8d8cf89399d171eecb53a425f2e93
Gabe Fierro <gtfierro225@gmail.com>
2014-08-08 10:44:24 -0700

    add virtual light sensor

---
+++ python/smap/drivers/sensors/virtualLightsensor.py
@@ -0,0 +1,23 @@
+import math
+from smap import driver
+from smap.util import periodicSequentialCall
+
+class VirtualLightSensor(driver.SmapDriver):
+    def setup(self, opts):
+        self.luxreading = float(opts.get('initiallux',40))
+        self.rate = float(opts.get('rate','1'))
+        self.add_timeseries('/illumination','Lx', data_type='double')
+
+        self.t = 1
+
+    def start(self):
+        periodicSequentialCall(self.read).start(self.rate)
+
+    def read(self):
+        self.t += 1
+        self.luxreading += .05 * math.cos(.001 * self.t)
+        self.add('/illumination',float("{0:.2f}".format(self.luxreading)))
+
+
+
+
