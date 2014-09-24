d2678fc2b2aff87619344e490a2659197d9745b8
Gabe Fierro <gtfierro225@gmail.com>
2014-09-23 19:28:05 -0700

    add ct80 driver

--- python/smap/drivers/thermostats/ct80.py
+++ python/smap/drivers/thermostats/ct80.py
@@ -3,7 +3,7 @@ Copyright (c) 2011, 2012, Regents of the University of California
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions 
+modification, are permitted provided that the following conditions
 are met:
 
  - Redistributions of source code must retain the above copyright
@@ -15,20 +15,22 @@ are met:
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
-FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
-THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 """
 @author Tyler Hoyt <thoyt@berkeley.edu>
+@author Gabe Fierro <gt.fierro@berkeley.edu>
 """
+
 from smap.driver import SmapDriver
 from smap.util import periodicSequentialCall
 from smap.contrib import dtutil
@@ -40,93 +42,72 @@ import time
 from twisted.internet import threads
 
 class CT80(SmapDriver):
+    """
+    The CT80 has a quirk in its API in which writing to t_heat (temp_heat) will change the mode of the thermostat to HEAT
+    as well as changing the heating setpoint. This has the effect of ignoring any cooling setpoint, and the thermostat will
+    not switch modes if it needs to. This is also true for setting t_cool (temp_cool).
+
+    We solve this by having an internal cache of the latest temp_heat and temp_cool setpoints as seen by POST requests to the
+    sMAP driver. Everytime the `read` function is run, we compare the current temperature to our heating and cooling setpoints
+    and use the CT80's API to set either t_heat or t_cool, depending on which setpoint the current temp reading is closest to
+    """
     def setup(self, opts):
         self.tz = opts.get('Timezone', 'America/Los_Angeles')
-        self.rate = float(opts.get('Rate', 1))
+        self.rate = float(opts.get('Rate', 5))
         self.ip = opts.get('ip', None)
-
+        self._setpoints = {'t_heat': None,
+                           't_cool': None}
+        self._sp_actuators = {'temp_heat': None,
+                              'temp_cool': None}
+    
+        # driver-specific metadata
         self.set_metadata('/', {'Metadata/Device': 'Thermostat',
                                 'Metadata/Model': 'CT80 RTA',
                                 'Metadata/Driver': __name__})
-         
+        # list of API points
         self.points = [
-                         {"name": "temp", "unit": "F", "data_type": "double"},
-                         {"name": "tmode", "unit": "Mode", "data_type": "long"},
-                         {"name": "tstate", "unit": "State", "data_type": "long"},
-                         {"name": "fmode", "unit": "Mode", "data_type": "long"},
-                         {"name": "fstate", "unit": "State", "data_type": "long"},
-                         {"name": "override", "unit": "Mode", "data_type": "long"},
-                         {"name": "hold", "unit": "Mode", "data_type": "long"},
-                         {"name": "t_heat", "unit": "F", "data_type": "double"},
-                         {"name": "t_cool", "unit": "F", "data_type": "double"},
-                         {"name": "program_mode", "unit": "Mode", "data_type": "long"}
-                       ]
-
+                        {"smapname": "temp", "name": "temp", "unit": "F", "data_type": "double"},
+                        {"smapname": "hvac_mode", "name": "tmode", "unit": "Mode", "data_type": "long"},
+                        {"smapname": "hvac_state", "name": "tstate", "unit": "State", "data_type": "long"},
+                        {"smapname": "fan_mode", "name": "fmode", "unit": "Mode", "data_type": "long"},
+                        {"smapname": "fan_state", "name": "fstate", "unit": "State", "data_type": "long"},
+                        {"smapname": "override", "name": "override", "unit": "Mode", "data_type": "long"},
+                        {"smapname": "hold", "name": "hold", "unit": "Mode", "data_type": "long"},
+                        {"smapname": "temp_heat", "name": "t_heat", "unit": "F", "data_type": "double"},
+                        {"smapname": "temp_cool", "name": "t_cool", "unit": "F", "data_type": "double"},
+                        {"smapname": "program_mode", "name": "program_mode", "unit": "Mode", "data_type": "long"}
+                      ]
         self.actuators = [
-            {"name": "t_heat", "act_type": "continuous", "unit": "F", "data_type": "double", "range": (40,100)},
-            {"name": "t_cool", "act_type": "continuous", "unit": "F", "data_type": "double", "range": (40,100)},
-            {"name": "tmode", "act_type": "discrete", "unit": "F", "data_type": "long", "states": [0,1,2,3]},
-            {"name": "fmode", "act_type": "discrete", "unit": "F", "data_type": "long", "states": [0,1]},
-            {"name": "override", "act_type": "discrete", "unit": "F", "data_type": "long", "states": [0,1]},
-            {"name": "hold", "act_type": "discrete", "unit": "F", "data_type": "long", "states": [0,1]},
-            {"name": "program_mode", "act_type": "discrete", "unit": "F", "data_type": "double", "states": [0,1]},
+            {"smapname": "temp_heat", "name": "t_heat", "act_type": "continuous", "unit": "F", "data_type": "double", "range": (40,100)},
+            {"smapname": "temp_cool", "name": "t_cool", "act_type": "continuous", "unit": "F", "data_type": "double", "range": (40,100)},
+            {"smapname": "hvac_mode", "name": "tmode", "act_type": "discrete", "unit": "F", "data_type": "long", "states": [0,1,2,3]},
+            {"smapname": "fan_mode", "name": "fmode", "act_type": "discrete", "unit": "F", "data_type": "long", "states": [0,1]},
+            {"smapname": "override", "name": "override", "act_type": "discrete", "unit": "F", "data_type": "long", "states": [0,1]},
+            {"smapname": "hold", "name": "hold", "act_type": "discrete", "unit": "F", "data_type": "long", "states": [0,1]},
+            {"smapname": "program_mode", "name": "program_mode", "act_type": "discrete", "unit": "F", "data_type": "double", "states": [0,1]},
           ]
 
-        # dictionary to translate the CT80 API endpoints into those defined by thermostat_functions.txt
-        self.translate = {
-                'temp': 'temp',
-                'tmode': 'hvac_mode',
-                'fmode': 'fan_mode',
-                'fstate': 'fan_state',
-                'override': 'override',
-                'hold': 'hold',
-                't_heat': 'temp_heat',
-                't_cool': 'temp_cool',
-                'tstate': 'hvac_state',
-                'program_mode': 'program_mode'
-                }
-
         ts = {}
+        # add timeseries
         for p in self.points:
-            ts[p['name']] = self.add_timeseries('/' + self.translate[p["name"]], p["unit"], data_type=p["data_type"], timezone=self.tz)
+            ts[p["smapname"]] = self.add_timeseries('/' + p["smapname"], p["unit"], data_type=p["data_type"], timezone=self.tz)
 
         # points not in the root resource
         self.add_timeseries('/humidity', '%RH', data_type="double")
 
-        
+        # instantiate actuators
         for a in self.actuators:
-            setup = {'ip': self.ip}
-            setup["name"] = a["name"]
+            setup = {'ip': self.ip, 'driver': self, 'name': a['name']}
             if a["act_type"] == "discrete":
                 setup["states"] = a["states"]
                 act = DiscreteActuator(**setup)
-                ts[a['name']].add_actuator(act)
+                ts[a['smapname']].add_actuator(act)
             elif a["act_type"] == "continuous":
                 setup["range"] = a["range"]
                 act = ContinuousActuator(**setup)
-                ts[a['name']].add_actuator(act)
-            else:
-                print 'invalid actuator type'
-                continue
-
-        # setup metadata for each timeseries
-        metadata_type = [
-                ('/temp','Sensor'),
-                ('/humidity','Sensor'),
-                ('/temp_heat','Reading'),
-                ('/temp_heat_act','SP'),
-                ('/temp_cool','Reading'),
-                ('/temp_cool_act','SP'),
-                ('/hold','Reading'),
-                ('/hold_act','Command'),
-                ('/override','Reading'),
-                ('/override_act','Command'),
-                ('/hvac_mode','Reading'),
-                ('/hvac_mode_act','Command')
-            ]
-        for ts, tstype in metadata_type:
-            self.set_metadata(ts,{'Metadata/Type':tstype})
-
+                if a['smapname'] in ['temp_heat', 'temp_cool']:
+                    self._sp_actuators[a['smapname']] = act
+                ts[a['smapname']].add_actuator(act)
 
     def start(self):
         # call self.read every self.rate seconds
@@ -136,15 +117,27 @@ class CT80(SmapDriver):
         url = 'http://' + self.ip + "/tstat"
         r = requests.get(url)
         if not r.ok:
+            print 'got status code',r.status_code,'from api'
             return
         vals = json.loads(r.text)
-         
+
         for p in self.points:
             if p['name'] not in vals: # sometimes the ct80 hiccups and doesn't give data
-                return
+                continue
             if type(vals[p['name']]) not in [int, float]:
                 return
-            self.add('/' + self.translate[p["name"]], vals[p["name"]])
+            self.add('/' + p["smapname"], vals[p["name"]])
+
+        # check which setpoint to write: if current temp is closer to heating setpoing,
+        # set t_heat, else set t_cool
+        if self._setpoints['t_heat'] is not None and self._setpoints['t_cool'] is not None:
+            if abs(self._setpoints['t_heat'] - vals['temp']) < abs(self._setpoints['t_cool'] - vals['temp']):
+                print 'Writing temp_heat', self._setpoints['t_heat']
+                self._sp_actuators['temp_heat'].set_state(None, self._setpoints['t_heat'])
+            else:
+                print 'Writing temp_cool', self._setpoints['t_cool']
+                self._sp_actuators['temp_cool'].set_state(None, self._setpoints['t_cool'])
+            
 
         r = requests.get(url + '/humidity')
         val = json.loads(r.text)
@@ -154,18 +147,25 @@ class _CT80Actuator(actuate.SmapActuator):
     def __init__(self, **opts):
         self.ip = opts.get('ip', None)
         self.name = opts.get('name', None)
+        self.driver = opts.get('driver')
         self.url = 'http://' + self.ip + '/tstat'
 
     def get_state(self, request):
         r = requests.get(self.url)
         rv = json.loads(r.text)
         return self.parse_state(rv.get(self.name,-1))
- 
+
     def set_state(self, request, state):
+        # if we get a web request for temp_heat or temp_cool, we cache it but
+        # don't actuate the CT80. To write temp_heat or temp_cool to the CT80,
+        # we have to have request == None
+        if request and self.name in ['t_heat','t_cool']:
+            print 'caching',self.name,state
+            self.driver._setpoints[self.name] = state
+            return
         payload = '{"' + self.name + '": ' + str(state) + '}'
         r = requests.post(self.url, data=payload)
-        time.sleep(2)
-        return state 
+        return state
 
 class DiscreteActuator(_CT80Actuator, actuate.NStateActuator):
     def __init__(self, **opts):
@@ -175,5 +175,4 @@ class DiscreteActuator(_CT80Actuator, actuate.NStateActuator):
 class ContinuousActuator(_CT80Actuator, actuate.ContinuousActuator):
     def __init__(self, **opts):
         actuate.ContinuousActuator.__init__(self, opts['range'])
-        _CT80Actuator.__init__(self, **opts) 
-
+        _CT80Actuator.__init__(self, **opts)
---
+++ python/smap/drivers/thermostats/ct80_old.py
@@ -0,0 +1,179 @@
+"""
+Copyright (c) 2011, 2012, Regents of the University of California
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions 
+are met:
+
+ - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+ - Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the
+   distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+"""
+@author Tyler Hoyt <thoyt@berkeley.edu>
+"""
+from smap.driver import SmapDriver
+from smap.util import periodicSequentialCall
+from smap.contrib import dtutil
+import requests
+import json
+from smap import actuate
+import time
+
+from twisted.internet import threads
+
+class CT80(SmapDriver):
+    def setup(self, opts):
+        self.tz = opts.get('Timezone', 'America/Los_Angeles')
+        self.rate = float(opts.get('Rate', 1))
+        self.ip = opts.get('ip', None)
+
+        self.set_metadata('/', {'Metadata/Device': 'Thermostat',
+                                'Metadata/Model': 'CT80 RTA',
+                                'Metadata/Driver': __name__})
+         
+        self.points = [
+                         {"name": "temp", "unit": "F", "data_type": "double"},
+                         {"name": "tmode", "unit": "Mode", "data_type": "long"},
+                         {"name": "tstate", "unit": "State", "data_type": "long"},
+                         {"name": "fmode", "unit": "Mode", "data_type": "long"},
+                         {"name": "fstate", "unit": "State", "data_type": "long"},
+                         {"name": "override", "unit": "Mode", "data_type": "long"},
+                         {"name": "hold", "unit": "Mode", "data_type": "long"},
+                         {"name": "t_heat", "unit": "F", "data_type": "double"},
+                         {"name": "t_cool", "unit": "F", "data_type": "double"},
+                         {"name": "program_mode", "unit": "Mode", "data_type": "long"}
+                       ]
+
+        self.actuators = [
+            {"name": "t_heat", "act_type": "continuous", "unit": "F", "data_type": "double", "range": (40,100)},
+            {"name": "t_cool", "act_type": "continuous", "unit": "F", "data_type": "double", "range": (40,100)},
+            {"name": "tmode", "act_type": "discrete", "unit": "F", "data_type": "long", "states": [0,1,2,3]},
+            {"name": "fmode", "act_type": "discrete", "unit": "F", "data_type": "long", "states": [0,1]},
+            {"name": "override", "act_type": "discrete", "unit": "F", "data_type": "long", "states": [0,1]},
+            {"name": "hold", "act_type": "discrete", "unit": "F", "data_type": "long", "states": [0,1]},
+            {"name": "program_mode", "act_type": "discrete", "unit": "F", "data_type": "double", "states": [0,1]},
+          ]
+
+        # dictionary to translate the CT80 API endpoints into those defined by thermostat_functions.txt
+        self.translate = {
+                'temp': 'temp',
+                'tmode': 'hvac_mode',
+                'fmode': 'fan_mode',
+                'fstate': 'fan_state',
+                'override': 'override',
+                'hold': 'hold',
+                't_heat': 'temp_heat',
+                't_cool': 'temp_cool',
+                'tstate': 'hvac_state',
+                'program_mode': 'program_mode'
+                }
+
+        ts = {}
+        for p in self.points:
+            ts[p['name']] = self.add_timeseries('/' + self.translate[p["name"]], p["unit"], data_type=p["data_type"], timezone=self.tz)
+
+        # points not in the root resource
+        self.add_timeseries('/humidity', '%RH', data_type="double")
+
+        
+        for a in self.actuators:
+            setup = {'ip': self.ip}
+            setup["name"] = a["name"]
+            if a["act_type"] == "discrete":
+                setup["states"] = a["states"]
+                act = DiscreteActuator(**setup)
+                ts[a['name']].add_actuator(act)
+            elif a["act_type"] == "continuous":
+                setup["range"] = a["range"]
+                act = ContinuousActuator(**setup)
+                ts[a['name']].add_actuator(act)
+            else:
+                print 'invalid actuator type'
+                continue
+
+        # setup metadata for each timeseries
+        metadata_type = [
+                ('/temp','Sensor'),
+                ('/humidity','Sensor'),
+                ('/temp_heat','Reading'),
+                ('/temp_heat_act','SP'),
+                ('/temp_cool','Reading'),
+                ('/temp_cool_act','SP'),
+                ('/hold','Reading'),
+                ('/hold_act','Command'),
+                ('/override','Reading'),
+                ('/override_act','Command'),
+                ('/hvac_mode','Reading'),
+                ('/hvac_mode_act','Command')
+            ]
+        for ts, tstype in metadata_type:
+            self.set_metadata(ts,{'Metadata/Type':tstype})
+
+
+    def start(self):
+        # call self.read every self.rate seconds
+        periodicSequentialCall(self.read).start(self.rate)
+
+    def read(self):
+        url = 'http://' + self.ip + "/tstat"
+        r = requests.get(url)
+        if not r.ok:
+            return
+        vals = json.loads(r.text)
+         
+        for p in self.points:
+            if p['name'] not in vals: # sometimes the ct80 hiccups and doesn't give data
+                return
+            if type(vals[p['name']]) not in [int, float]:
+                return
+            self.add('/' + self.translate[p["name"]], vals[p["name"]])
+
+        r = requests.get(url + '/humidity')
+        val = json.loads(r.text)
+        self.add('/humidity', val['humidity'])
+
+class _CT80Actuator(actuate.SmapActuator):
+    def __init__(self, **opts):
+        self.ip = opts.get('ip', None)
+        self.name = opts.get('name', None)
+        self.url = 'http://' + self.ip + '/tstat'
+
+    def get_state(self, request):
+        r = requests.get(self.url)
+        rv = json.loads(r.text)
+        return self.parse_state(rv.get(self.name,-1))
+ 
+    def set_state(self, request, state):
+        payload = '{"' + self.name + '": ' + str(state) + '}'
+        r = requests.post(self.url, data=payload)
+        time.sleep(2)
+        return state 
+
+class DiscreteActuator(_CT80Actuator, actuate.NStateActuator):
+    def __init__(self, **opts):
+        actuate.NStateActuator.__init__(self, opts['states'])
+        _CT80Actuator.__init__(self, **opts)
+
+class ContinuousActuator(_CT80Actuator, actuate.ContinuousActuator):
+    def __init__(self, **opts):
+        actuate.ContinuousActuator.__init__(self, opts['range'])
+        _CT80Actuator.__init__(self, **opts) 
+
